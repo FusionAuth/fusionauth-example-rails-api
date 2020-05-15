03e27039469d8d0d5a4c5c919fd11679ea0006be
Dan Moore <github@mooreds.com>
2020-05-15 21:49:07 +0000

    Introduced testing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  #before_action :require_jwt
+
+  #def require_jwt
+    #head :forbidden
+  #end
 end
--- test/controllers/message_controller_test.rb
+++ test/controllers/message_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class MessageControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can get messages" do
+    get "/messages"
+    assert_response :success
+  end
+  test "can get messages content" do
+    get "/messages"
+    res = JSON.parse(@response.body)
+    assert_equal '{"messages"=>["Hello"]}', res.to_s
+  end
 end
