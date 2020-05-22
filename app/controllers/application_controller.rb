695dfe39fe9f884b093a6eb795ea5328a3b045c4
Dan Moore <github@mooreds.com>
2020-05-22 20:21:45 +0000

    Verify claims.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,15 @@ class ApplicationController < ActionController::API
       return false
     end
 
+    expected_iss = 'fusionauth.io'
+    expected_aud = '238d4793-70de-4183-9707-48ed8ecd19d9'
+
     token.gsub!('Bearer ','')
     begin
-      decoded_token = JWT.decode token, Rails.configuration.x.oauth.jwt_secret, true
+      decoded_token = JWT.decode token, Rails.configuration.x.oauth.jwt_secret, true, { verify_iss: true, iss: expected_iss, verify_aud: true, aud: expected_aud, algorithm: 'HS256' }
       return true
-    rescue JWT::DecodeError
-      
+    rescue JWT::DecodeError => e
+      Rails.logger.warn "Error decoding the JWT: "+ e.to_s
     end
     false
   end
--- test/integration/messages_test.rb
+++ test/integration/messages_test.rb
@@ -13,21 +13,40 @@ class MessagesTest < ActionDispatch::IntegrationTest
     get "/messages", headers: { "HTTP_AUTHORIZATION" => "Bearer " + build_jwt(-1) }
     assert_response :forbidden
   end
+  test "incorrect aud fails" do
+    get "/messages", headers: { "HTTP_AUTHORIZATION" => "Bearer " + build_jwt(5, 'WeAreNotTheIntendedAudience') }
+    assert_response :forbidden
+  end
+  test "incorrect iss fails" do
+    get "/messages", headers: { "HTTP_AUTHORIZATION" => "Bearer " + build_jwt(5, "238d4793-70de-4183-9707-48ed8ecd19d9", "NotTheIssuerWeExpected") }
+    assert_response :forbidden
+  end
+  test "nbf fails" do
+    get "/messages", headers: { "HTTP_AUTHORIZATION" => "Bearer " + build_jwt(15, "238d4793-70de-4183-9707-48ed8ecd19d9", "fusionauth.io", 10 ) }
+    assert_response :forbidden
+  end
   test "can get messages content" do
     get "/messages", headers: { "HTTP_AUTHORIZATION" => "Bearer " + build_jwt }
     res = JSON.parse(@response.body)
     assert_equal '{"messages"=>["Hello"]}', res.to_s
   end
 
-  def build_jwt(valid_for_minutes = 5)
+  def build_jwt(valid_for_minutes = 5, 
+                aud = "238d4793-70de-4183-9707-48ed8ecd19d9",
+                iss = "fusionauth.io", 
+                not_valid_before_x_minutes_from_now = nil)
     exp = Time.now.to_i + (valid_for_minutes*60)
-    payload = { "iss": "fusionauth.io",
+    payload = { "iss": iss,
                 "exp": exp,
-                "aud": "238d4793-70de-4183-9707-48ed8ecd19d9",
+                "aud": aud,
                 "sub": "19016b73-3ffa-4b26-80d8-aa9287738677",
                 "name": "Dan Moore",
                 "roles": ["USER"]
     }
+    if not_valid_before_x_minutes_from_now then
+      nbf = Time.now.to_i + (valid_for_minutes*60)
+      payload["nbf"] = nbf
+    end
     
     JWT.encode payload, Rails.configuration.x.oauth.jwt_secret, 'HS256'
 
